138b9f1bb37096eab828e4ce9d81d31757d9d31c
leonardo <leonardocave934@gmail.com>
2023-06-23 18:42:19 -0300

    primeira,segunda e terceira aulas de React JS

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+  <header>
+ <span>PobreFlix</span>
+ <nav>
+    <a href="#">Home</a>
+    <a href="#">Assistir</a>
+ </nav>
+  </header>
+  );
+}
+
+export default Header;
